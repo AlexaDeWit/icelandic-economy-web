bda8d53b208d32bc081dfea6417e9a3cd159b812
Alexa DeWit <alexa.dewit@gmail.com>
2015-07-20 17:04:11 +0200

    Use Applicative Functor, or that's what they said it was!

--- app/controllers/OAuthClient.scala
+++ app/controllers/OAuthClient.scala
@@ -3,6 +3,8 @@ package controllers
 import play.api._
 import play.api.mvc._
 import scalaz._
+import scalaz.std.option._
+import scalaz.syntax.apply._
 import alexadewit_on_github.icelandic_economy._
 import alexadewit_on_github.icelandic_economy.oauth2._
 import org.http4s.Uri.uri
@@ -15,32 +17,29 @@ class OAuthClient extends Controller {
       val queryString = EveOnlineOAuth.loginQueryString(
         keys, stateToken, List("publicData"), "http://localhost:9000/oauth_client/callback"
       )
-      Redirect(
-        s"${EveOnlineOAuth.authUrl}?${queryString}",
-        EveOnlineOAuth.redirectCode ).withSession( "oauth2state" -> stateToken )
+    Redirect(
+      s"${EveOnlineOAuth.authUrl}?${queryString}",
+      EveOnlineOAuth.redirectCode ).withSession( "oauth2state" -> stateToken )
     })
   }
 
   def callback( code: Option[String], state: Option[String] )  = Action { implicit request =>
-    ( code, state ) match {
-      case ( None, _ ) | ( _ , None ) => Ok( views.html.index("FUCK!") )
-      case ( Some(authCode), Some( stateToken ) ) => {
-        withOAuthKeysOrNotFound( OAuth2Keys.default )( keys => {
-          val request = OAuth2.accessTokenRequest(
-            uri("https://login.eveonline.com/oauth/token"),
+    ( code |@| state ) { case ( authCode, stateToken ) => 
+      withOAuthKeysOrNotFound( OAuth2Keys.default )( keys => {
+        val request = OAuth2.accessTokenRequest(
+          uri("https://login.eveonline.com/oauth/token"),
             authCode,
             keys
           )
-          OAuth2.accessToken( request ).run.fold( s => {
-            println( s )
-            InternalServerError(
-              "Received an invalid response from Eve Online's OAuth2 Service." 
-            )},
-            token => Ok( views.html.index( token.toString ) )
-          )
-        })
-      }
-    }
+        OAuth2.accessToken( request ).run.fold( s => {
+          println( s )
+          InternalServerError(
+            "Received an invalid response from Eve Online's OAuth2 Service." 
+        )},
+      token => Ok( views.html.index( token.toString ) )
+      )
+      })
+    } getOrElse ( NotFound( "REKT CUNT" ) )
   }
 
   def withOAuthKeysOrNotFound( possibleKeys: String \/ OAuth2Keys )( f: OAuth2Keys => Result ) : Result = {
