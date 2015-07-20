539bc31207f601097db2b2aaf5fc546c74431cc0
Alexa DeWit <alexa.dewit@gmail.com>
2015-07-20 15:56:12 +0200

    Cleanup

--- app/controllers/OAuthClient.scala
+++ app/controllers/OAuthClient.scala
@@ -9,19 +9,15 @@ import alexadewit_on_github.icelandic_economy.oauth2._
 class OAuthClient extends Controller {
 
   def authorize = Action { implicit request =>
-    OAuth2Keys.default match {
-      case \/-( keys ) => {
-        val stateToken = TokenGenerator.randomToken( 30 )
-        val queryString = EveOnlineOAuth.loginQueryString(
-          keys, stateToken, List("publicData"), "http://localhost:9000/oauth_client/callback"
-        )
-        Redirect(
-          s"${EveOnlineOAuth.authUrl}?${queryString}",
-          EveOnlineOAuth.redirectCode
-          ).withSession( "oauth2state" -> stateToken )
-      }
-      case _ => NotFound( "OAuth2 Client not configured" )
-    }
+    withOAuthKeysOrNotFound( keys => {
+      val stateToken = TokenGenerator.randomToken( 30 )
+      val queryString = EveOnlineOAuth.loginQueryString(
+        keys, stateToken, List("publicData"), "http://localhost:9000/oauth_client/callback"
+      )
+      Redirect(
+        s"${EveOnlineOAuth.authUrl}?${queryString}",
+        EveOnlineOAuth.redirectCode ).withSession( "oauth2state" -> stateToken )
+    })
   }
 
   def callback( code: Option[String], state: Option[String] )  = Action { implicit request =>
@@ -31,6 +27,10 @@ class OAuthClient extends Controller {
         Ok( views.html.index( s"${EveOnlineOAuth.getAccessToken(authCode, OAuth2Keys.default.toOption.get)}" ) )
       }
     }
+  }
+
+  def withOAuthKeysOrNotFound( f: OAuth2Keys => Result ) : Result = {
+    OAuth2Keys.default.fold( s => NotFound( "OAuth2 Client not configured." ) , f )
   }
 
 }
