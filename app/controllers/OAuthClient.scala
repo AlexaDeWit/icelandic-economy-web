6541650ee257afc2d930d0b24c38791c754caea8
Alexa DeWit <alexa.dewit@gmail.com>
2015-07-24 18:18:28 +0200

    maybe more readable

--- app/controllers/OAuthClient.scala
+++ app/controllers/OAuthClient.scala
@@ -16,10 +16,12 @@ class OAuthClient extends Controller {
 
   def authorize = Action { implicit request =>
     withOAuthKeysOrNotFound( OAuth2Keys.default )( keys => {
+
       val stateToken = TokenGenerator.randomToken( 30 )
       val queryString = EveOnlineOAuth.loginQueryString(
         keys, stateToken, List("publicData"), "http://localhost:9000/oauth_client/callback"
       )
+
       Redirect(
         s"${EveOnlineOAuth.authUrl}?${queryString}",
         EveOnlineOAuth.redirectCode 
@@ -29,15 +31,24 @@ class OAuthClient extends Controller {
 
   def callback( code: Option[String], state: Option[String] ) = Action { implicit request =>
     ( code |@| state ) { case ( authCode, stateToken ) => 
+
       withOAuthKeysOrNotFound( OAuth2Keys.default )( keys => {
-        EveOnlineOAuth.accessTokenRequest( authCode, keys ).run.fold( s => {
-          InternalServerError(
-            "Received an invalid response from Eve Online's OAuth2 Service." 
-        )},
-          token => Ok( views.html.index( "Sent;") ).withSession(
-            "accessToken" -> token.asJson.toString,
-            "tokenCreated" -> DateTime.now.toString
-          )
+
+        EveOnlineOAuth.accessTokenRequest( authCode, keys ).run.fold(
+
+          s => {
+            InternalServerError(
+              "Received an invalid response from Eve Online's OAuth2 Service." 
+            )
+          },
+
+          token => {
+            Ok( views.html.index( "Sent;") ).withSession(
+              "accessToken" -> token.asJson.toString,
+              "tokenCreated" -> DateTime.now.toString
+            )
+          }
+
         )
       })
     } getOrElse ( NotFound( "REKT CUNT" ) )
